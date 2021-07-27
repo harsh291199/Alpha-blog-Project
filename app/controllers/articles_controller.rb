05c9f0330e0f6b72c01e69b3901157f3687cf654
harsh291199 <harshsuthar2911@gmail.com>
2021-07-27 13:21:28 +0530

    Add username to index and show views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,21 +2,19 @@
 
 # Articles Controller
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: %i[show edit update destroy]
 
   def index
     @articles = Article.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @article = Article.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @article = Article.new(article_params)
